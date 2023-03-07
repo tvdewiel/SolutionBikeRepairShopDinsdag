ba6f7d276b74cac515d20ffdf99920cdefdcd77d
Tom Vande Wiele <tvdewiel@hotmail.com>
2023-03-07 08:42:38 +0100

    up

---
+++ ConsoleAppTestDL/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BikeRepairShop.DL.Repositories;
+
+Console.WriteLine("Hello, World!");
+string conn = "Data Source=NB21-6CDPYD3\\SQLEXPRESS;Initial Catalog=BikeRepairShopDinsdag;Integrated Security=True";
+CustomerRepository repo = new CustomerRepository(conn);
+var bikes=repo.GetBikesInfo();
+Console.WriteLine("end");
